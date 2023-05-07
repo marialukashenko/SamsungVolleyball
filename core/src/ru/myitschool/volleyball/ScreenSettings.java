ad503479c087fd69c91a5ee1df53985030abf737
PolinaMikhaylova <polisha1602@gmail.com>
2023-05-07 13:15:00 +0600

    beautiful code <3

--- core/src/ru/myitschool/volleyball/ScreenSettings.java
+++ core/src/ru/myitschool/volleyball/ScreenSettings.java
@@ -1,14 +1,21 @@
 package ru.myitschool.volleyball;
 
+import static ru.myitschool.volleyball.MyGdx.SCR_HEIGHT;
+import static ru.myitschool.volleyball.MyGdx.SCR_WIDTH;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 
 public class ScreenSettings implements Screen {
     MyGdx gdx;
     Texture imgBackGround;
+    TextButton btnBack;
 
     public ScreenSettings(MyGdx myGdx) {
         gdx = myGdx;
+        imgBackGround = new Texture("background.jpg");
+        btnBack = new TextButton(gdx.fontLarge, "BACK", SCR_WIDTH*100-200, SCR_HEIGHT*100-30);
     }
 
     @Override
@@ -18,7 +25,24 @@ public class ScreenSettings implements Screen {
 
     @Override
     public void render(float delta) {
+        if (Gdx.input.justTouched()) {
+            gdx.touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            gdx.camera.unproject(gdx.touch);
+            if (btnBack.hit(gdx.touch.x, gdx.touch.y)) {
+                gdx.setScreen(gdx.screenIntro);
+            }
+        }
 
+        //отрисовка
+        gdx.camera.update();
+        gdx.batch.setProjectionMatrix(gdx.camera.combined);
+        gdx.batch.begin();
+        gdx.batch.draw(imgBackGround, 0, 0, SCR_WIDTH, SCR_HEIGHT);
+        gdx.batch.end();
+        gdx.batch.setProjectionMatrix(gdx.camera2.combined);
+        gdx.batch.begin();
+        btnBack.font.draw(gdx.batch, btnBack.text, btnBack.x, btnBack.y);
+        gdx.batch.end();
     }
 
     @Override
@@ -43,7 +67,7 @@ public class ScreenSettings implements Screen {
 
     @Override
     public void dispose() {
-
+        imgBackGround.dispose();
     }
 }
 
